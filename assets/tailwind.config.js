ebc8002f6504cb33b41ce6a9d380ce4bf1f2f569
oleksandr.chernyonkov <oleksandr.chernyonkov@introduct.tech>
2024-02-22 15:11:58 +0200

    start commit

---
+++ assets/tailwind.config.js
@@ -0,0 +1,16 @@
+// assets/tailwind.config.js
+
+const tailwind_config = require("../deps/moon/assets/tailwind.config.js");
+
+tailwind_config.content = [
+  "../lib/**/*.ex",
+  "../lib/**/*.heex",
+  "../lib/**/*.eex",
+  "./js/**/*.js",
+
+  "../deps/moon/lib/**/*.ex",
+  "../deps/moon/lib/**/*.heex",
+  "../deps/moon/lib/**/*.eex",
+  "../deps/moon/assets/js/**/*.js",
+];
+module.exports = tailwind_config;
